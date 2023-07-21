b0c848e7998713e085538f019d639a765058bbf5
Maouai233 <maouai233@outlook.com>
2023-07-21 22:31:01 +0800

    [bili] 003 基础调试宏

--- Src/SigmaBootPkg/Main.c
+++ Src/SigmaBootPkg/Main.c
@@ -2,16 +2,31 @@
 
 #include <Boot/Boot.h>
 
+VOID RetVoid()
+{
+    ERR_RET (EFI_OUT_OF_RESOURCES);
+    DEBUG ((DEBUG_INFO ,"This will not be go there later\n"));
+}
+
+EFI_STATUS RetStatus()
+{
+    ERR_RETS (EFI_INVALID_PARAMETER);
+    DEBUG ((DEBUG_INFO ,"This will not be go there later\n"));
+    return EFI_SUCCESS;
+}
+
 EFI_STATUS EFIAPI UefiMain (
         IN EFI_HANDLE        ImageHandle,
         IN EFI_SYSTEM_TABLE  *SystemTable
         )
 {
     Breakpoint();
-    SystemTable->ConOut->OutputString (
-            SystemTable->ConOut,
-            L"Hello world!\n"
-            );
+
+    RetVoid();
+    RetStatus();
+
+    DEBUG ((DEBUG_INFO,"Test DEBUG Macro !!!\n"));
+    ASSERT (1 != 1);
 
     return EFI_SUCCESS;
 }
