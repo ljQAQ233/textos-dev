ljQAQ233/textos-pre

[update] file : global files (shared offset and `file_t`)          vfs  : `O_CREATE` -> `VFS_CREATE`

src/kernel/fs/phy/fat32.c
@@ -1214,14 +1214,14 @@ static int fat32_open (node_t *parent, char *path, u64 Args, node_t **result)
     opened = fat32_pathwalk (ori, &path, &last);
     if (opened)
     {
-        if (opened->attr & NA_ARCHIVE && Args & O_DIR)
+        if (opened->attr & NA_ARCHIVE && Args & VFS_DIR)
             stat = -ENOTDIR;
-        else if (opened->attr & NA_DIR && ~Args & O_DIR)
+        else if (opened->attr & NA_DIR && ~Args & VFS_DIR)
             stat = -EISDIR;
         goto end;
     }
 
-    if (Args & O_CREATE)
+    if (Args & VFS_CREATE)
     {
         /*
            按照习惯, 默认只能在一个已经存在的目录下创建新的节点,
@@ -1233,7 +1233,7 @@ static int fat32_open (node_t *parent, char *path, u64 Args, node_t **result)
         }
 
         int attr = 0;
-        if (Args & O_DIR)
+        if (Args & VFS_DIR)
             attr |= NA_DIR;
         else
             attr |= NA_ARCHIVE;

ljQAQ233/textos-dev

src/kernel/fs/phy/fat32.c
@@ -1214,14 +1214,14 @@ static int fat32_open (node_t *parent, char *path, u64 args, node_t **result)
     opened = fat32_pathwalk (ori, &path, &last);
     if (opened)
     {
-        if (opened->attr & NA_REG && args & O_DIR)
+        if (opened->attr & NA_REG && args & VFS_DIR)
             ret = -ENOTDIR;
-        else if (opened->attr & NA_DIR && ~args & O_DIR)
+        else if (opened->attr & NA_DIR && ~args & VFS_DIR)
             ret = -EISDIR;
         goto end;
     }
 
-    if (args & O_CREATE)
+    if (args & VFS_CREATE)
     {
         /*
            按照习惯, 默认只能在一个已经存在的目录下创建新的节点,
@@ -1233,7 +1233,7 @@ static int fat32_open (node_t *parent, char *path, u64 args, node_t **result)
         }
 
         int attr = 0;
-        if (args & O_DIR)
+        if (args & VFS_DIR)
             attr |= NA_DIR;
         else
             attr |= NA_REG;