ljQAQ233/textos-pre

[bili] 032 串口 与 debugcon

src/kernel/debug.c
@@ -1,27 +1,68 @@
+#include <io.h>
+#include <textos/debug.h>
 #include <textos/args.h>
 #include <textos/printk.h>
 #include <textos/klib/vsprintf.h>
 
-static char buf[128];
+#define R_CON 0xe9
 
-void debugk (
-        const char *file,
-        const u64  line,
-        const char *format,
-        ...
-        )
+static inline void dputc(char c)
+{
+    outb(R_CON, c);
+}
+
+static void dputs(char *str)
+{
+    char *p = str;
+    while (*p)
+        dputc(*p++);
+}
+
+int dmask = 0;
+
+int dprintk_set(int mask)
+{
+    int old = dmask;
+
+    mask &= K_ALL; // drop
+    dmask = mask;
+    
+    return old;
+}
+
+// dprint
+
+static void dvprintk(int lv, const char *format, va_list ap)
+{
+    static char buf[128];
+    int mask = dmask | K_NMSK | K_SYNC;
+    if (mask & lv) {
+        vsprintf(buf, format, ap);
+        dputs(buf);
+        if (dmask & K_SYNC)
+            printk(buf);
+    }
+}
+
+void dprintk(int lv, const char *format, ...)
 {
     va_list args;
-    va_start (args, format);
+    va_start(args, format);
 
-    vsprintf (
-            buf,
-            format,
-            args
-        );
+    dvprintk(lv, format, args);
 
-    printk ("[%s:%d] %s", file, line, buf);
+    va_end(args);
+}
 
-    va_end (args);
+void debugk(int lv, const char *file, const int line, const char *format, ...)
+{
+    static char buf[128];
+    va_list args;
+    va_start(args, format);
+
+    vsprintf(buf, format, args);
+    dprintk(lv, "[%s:%d] %s", file, line, buf);
+
+    va_end(args);
 }
 

ljQAQ233/textos-dev

src/kernel/debug.c
@@ -1,27 +1,68 @@
+#include <io.h>
+#include <textos/debug.h>
 #include <textos/args.h>
 #include <textos/printk.h>
 #include <textos/klib/vsprintf.h>
 
-static char buf[128];
+#define R_CON 0xe9
 
-void debugk (
-        const char *file,
-        const u64  line,
-        const char *format,
-        ...
-        )
+static inline void dputc(char c)
+{
+    outb(R_CON, c);
+}
+
+static void dputs(char *str)
+{
+    char *p = str;
+    while (*p)
+        dputc(*p++);
+}
+
+int dmask = 0;
+
+int dprintk_set(int mask)
+{
+    int old = dmask;
+
+    mask &= K_ALL; // drop
+    dmask = mask;
+    
+    return old;
+}
+
+// dprint
+
+static void dvprintk(int lv, const char *format, va_list ap)
+{
+    char buf[128];
+    int mask = dmask | K_SYNC;
+    if (mask & lv) {
+        vsprintf(buf, format, ap);
+        dputs(buf);
+        if (lv & K_SYNC)
+            printk(buf);
+    }
+}
+
+void dprintk(int lv, const char *format, ...)
 {
     va_list args;
-    va_start (args, format);
+    va_start(args, format);
 
-    vsprintf (
-            buf,
-            format,
-            args
-        );
+    dvprintk(lv, format, args);
 
-    printk ("[%s:%d] %s", file, line, buf);
+    va_end(args);
+}
 
-    va_end (args);
+void debugk(int lv, const char *file, const int line, const char *format, ...)
+{
+    char buf[128];
+    va_list args;
+    va_start(args, format);
+
+    vsprintf(buf, format, args);
+    dprintk(lv, "[%s:%d] %s", file, line, buf);
+
+    va_end(args);
 }
 