2e2ab58dcf6bc60731f70a10b47496d486783bf7
maouai233 <maouai233@outlook.com>
2025-05-18 12:21:44 +0800

    [update] app/uname : linux compatibility

--- src/app/uname.c
+++ src/app/uname.c
@@ -1,21 +1,23 @@
 #include <app/api.h>
 #include <stdio.h>
 
+char buf[1 << 9];
+
 int main(int argc, char const *argv[])
 {
-    struct utsname u;
-    if (uname(&u) < 0)
+    struct utsname *u = (struct utsname *)buf;
+    if (uname(u) < 0)
     {
         perror("uname");
         return 1;
     }
 
     printf("%s %s %s %s %s\n",
-            u.sysname,
-            u.nodename,
-            u.release,
-            u.version,
-            u.machine);
+            u->sysname,
+            u->nodename,
+            u->release,
+            u->version,
+            u->machine);
     return 0;
 }
 
