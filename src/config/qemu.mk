6a0182ba8b0ac3a1b5470b52cd15b0af6fa0897f
maouai233 <maouai233@outlook.com>
2025-08-02 23:14:24 +0800

    [update] qemu : QEMU_GPY updated

--- src/config/qemu.mk
+++ src/config/qemu.mk
@@ -3,25 +3,28 @@ QEMU_BINARY = /usr/bin
 
 QEMU = $(QEMU_BINARY)/qemu-system-x86_64
 
-ifdef QEMU_LOG
-  QEMU_SERIAL = file:$(OUTPUT)/qemu.srl
-else
-  QEMU_SERIAL = stdio
-endif
-
 QEMU_LOG ?= file:$(OUTPUT)/qemu.log
 
 MEM = 64M
 
 # Qemu Common Args
-QEMU_FLAGS := -hda $(IMG) \
-			   -cpu qemu64,+x2apic \
-			   -m $(MEM) \
-			   -no-reboot \
-			   -debugcon $(QEMU_LOG) \
-			   -device isa-debug-exit \
-			   -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
-			   -device e1000,netdev=net0,mac=52:54:00:12:34:56
+QEMU_FLAGS := \
+  -hda $(IMG) \
+  -cpu qemu64,+x2apic \
+  -m $(MEM) \
+  -no-reboot \
+  -debugcon $(QEMU_LOG) \
+  -device isa-debug-exit \
+  -netdev tap,id=net0,ifname=tap0,script=no,downscript=no \
+  -device e1000,netdev=net0,mac=52:54:00:12:34:56
+
+# No graphic mode
+# TODO: make udk debugger use tcp to connect to QEMU rather than serial and named pipe
+ifeq (${QEMU_GPY},false)
+  ifneq (${BSRC_DEBUG},true)
+    QEMU_FLAGS += -nographic
+  endif
+endif
 
 # Firmware selection
 define fw_arg
@@ -29,25 +32,17 @@ define fw_arg
   -drive if=pflash,format=raw,file=$(BASE)/OVMF_$(1)_$(ARCH).vars
 endef
 
-QEMU_FLAGS_RUN  := $(QEMU_FLAGS) \
-				   $(call fw_arg,RELEASE)
+QEMU_FLAGS_RUN := \
+  $(QEMU_FLAGS) \
+  $(call fw_arg,RELEASE)
 
-QEMU_FLAGS_DBG  := $(QEMU_FLAGS)
+QEMU_FLAGS_DBG := $(QEMU_FLAGS)
 
-# No graphic for debugging
-ifeq (${QEMU_GPY},false)
-  ifeq (${BSRC_DEBUG},false)
-    QEMU_FLAGS_DBG += -nographic
-  endif
-endif
+QEMU_FLAGS_BDBG := \
+  $(QEMU_FLAGS_DBG) \
+  $(call fw_arg,NOOPT)
 
-QEMU_FLAGS_BDBG := $(QEMU_FLAGS_DBG) \
-				   $(call fw_arg,NOOPT)
-
-QEMU_FLAGS_KDBG := $(QEMU_FLAGS_DBG) \
-				   -s -S \
-				   -serial $(QEMU_SERIAL) \
-				   $(call fw_arg,RELEASE) \
-				   # -bios base/OVMF_RELEASE_X64.fd
-
-# Qemu Args for Debugging
+QEMU_FLAGS_KDBG := \
+  $(QEMU_FLAGS_DBG) \
+  -s -S \
+  $(call fw_arg,RELEASE)
