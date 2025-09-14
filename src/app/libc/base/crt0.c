0ef2cf260e2e11163080532c635bc3b4a9b8a4bf
maouai233 <maouai233@outlook.com>
2025-09-14 17:08:35 +0800

    [update] libc : use .init_array / .fini_array instead

--- src/app/libc/base/crt0.c
+++ src/app/libc/base/crt0.c
@@ -7,20 +7,36 @@ int main(int argc, const char *argv[], const char *envp[])
 }
 
 extern char **__environ;
-extern void __init_stdio();
-extern void __init_linker();
 
-void start0(long *args)
+typedef void (*fn)();
+extern fn __init_array_start[];
+extern fn __init_array_end[];
+extern fn __fini_array_start[];
+extern fn __fini_array_end[];
+
+static void libc_init()
+{
+    for (fn *f = __init_array_start; f < __init_array_end; f++)
+        (*f)();
+}
+
+static void libc_fini()
+{
+    for (fn *f = __fini_array_end - 1; f >= __fini_array_start; f--)
+        (*f)();
+}
+
+static void start0(long *args)
 {
     int argc = args[0];
     const char **argv = (void *)&args[1];
     const char **envp = (void *)&args[1+argc+1];
 
     __environ = (char **)envp;
-    __init_stdio();
-    __init_linker();
-
-    _exit(main(argc, argv, envp));
+    libc_init();
+    int ret = main(argc, argv, envp);
+    libc_fini();
+    _exit(ret);
 
     asm("ud2");
 }
