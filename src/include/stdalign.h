9199017b4eeb27cdfdf02ae003b311417b3f6079
maouai233 <maouai233@outlook.com>
2026-01-11 13:36:46 +0800

    [u/f] 1. added iso646.h / stdalign.h / stdnoreturn.h       2. fixed sigjmp_buf : type as an array (pointer)

---
+++ src/include/stdalign.h
@@ -0,0 +1,13 @@
+#ifndef _STDALIGN_H
+#define _STDALIGN_H
+
+#ifndef __cplusplus
+    // C11
+    #define alignas _Alignas
+    #define alignof _Alignof
+#endif
+
+#define __alignas_is_defined 1
+#define __alignof_is_defined 1
+
+#endif
---
+++ src/include/stdnoreturn.h
@@ -0,0 +1,8 @@
+#ifndef _STDNORETURN_H
+#define _STDNORETURN_H
+#ifndef __cplusplus
+    // C11
+    #include <sys/cdefs.h>
+    #define noreturn _Noreturn
+#endif
+#endif
