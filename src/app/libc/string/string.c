08066e977682377b8e9eaa93028bab2523c40e0a
maouai233 <maouai233@outlook.com>
2026-03-21 20:16:24 +0800

    [upd] Separate kernel and app libc modules, sync string.c, move ELF defs
    - Split kernel and app libc implementations for list, hlist, and htable.
    - Moved ELF definitions from kernel/elf.h to include/bits/elf.h.
    - Synchronized klib and libc string.c implementations.

--- src/app/libc/string/string.c
+++ src/app/libc/string/string.c
@@ -1,21 +1,14 @@
-#include <stdint.h>
-#include <string.h>
 #include <malloc.h>
 
-typedef uint8_t u8;
-
-#define max(A,B) (((A) > (B)) ? (A) : (B))
-#define min(A,B) (((A) < (B)) ? (A) : (B))
-
 /*
- * The `strlen()` function calculates the 
+ * The `strlen()` function calculates the
  * length of the string pointed to by `str`,
  * excluding the terminating null byte ('\0')
-*/
+ */
 size_t strlen(const char *str)
 {
-    size_t i =0;
-    
+    size_t i = 0;
+
     while (str && *str++)
         i++;
     return i;
@@ -27,7 +20,7 @@ size_t strlen(const char *str)
 size_t strnlen(const char *str, size_t maxlen)
 {
     size_t i = 0;
-    
+
     while (str && i < maxlen && *str++)
         i++;
     return i;
@@ -36,7 +29,7 @@ size_t strnlen(const char *str, size_t maxlen)
 /*
  * `strcpy()` copies the string pointed to by `src`,
  * including the terminating null byte ('\0')
-*/
+ */
 char *strcpy(char *dest, const char *src)
 {
     char *p = dest;
@@ -50,34 +43,32 @@ char *strcpy(char *dest, const char *src)
 /*
  * `strcmp()` compares the two strings `str1` and `str2`
  */
-int strcmp(const char* str1,const char *str2)
+int strcmp(const char *str1, const char *str2)
 {
     while (*str1 && *str2 && *str1 == *str2) {
         str1++;
         str2++;
     }
-    return (u8)*str1 - (u8)*str2;
+    return (unsigned char)*str1 - (unsigned char)*str2;
 }
 
 /*
  * The `strchr()` function returns a pointer to the first
  * occurrence of the character `c` in the string `str`
-*/
+ */
 char *strchr(const char *str, int c)
 {
     do {
-        if (*str == (char)c)
-            return (char *)str;
+        if (*str == (char)c) return (char *)str;
     } while (*str++);
-    return NULL;
+    return 0;
 }
 
 char *strrchr(const char *str, int c)
 {
-    char *res = NULL;
+    char *res = 0;
     do {
-        if (*str == (char)c)
-            res = (char *)str;
+        if (*str == (char)c) res = (char *)str;
     } while (*str++);
     return res;
 }
@@ -85,43 +76,39 @@ char *strrchr(const char *str, int c)
 char *strchrnul(const char *str, int c)
 {
     do {
-        if (*str == (char)c)
-            return (char *)str;
+        if (*str == (char)c) return (char *)str;
     } while (*str++);
     return (char *)(str - 1);
 }
 
 char *strrchrnul(const char *str, int c)
 {
-    const char *r = NULL;
+    const char *r = 0;
     do {
-        if (*str == (char)c)
+        if (*str == (char)c) {
             r = str;
+        }
     } while (*str++);
     return (char *)(r ? r : str - 1);
 }
 
 char *strstr(const char *haystack, const char *needle)
 {
-    if (*needle == '\0')
-        return (char *)haystack;
+    if (*needle == '\0') return (char *)haystack;
 
-    for (; *haystack != '\0'; ++haystack)
-    {
+    for (; *haystack != '\0'; ++haystack) {
         const char *h = haystack;
         const char *n = needle;
 
-        while (*h != '\0' && *n != '\0' && *h == *n)
-        {
+        while (*h != '\0' && *n != '\0' && *h == *n) {
             ++h;
             ++n;
         }
 
-        if (*n == '\0')
-            return (char *)haystack;
+        if (*n == '\0') return (char *)haystack;
     }
 
-    return NULL;
+    return 0;
 }
 
 // string span
@@ -175,8 +162,8 @@ char *strncpy(char *dest, const char *src, size_t n)
 /*
  * `strncmp()` is similar to `strcmp()`, except it
  * compares only the first (at most) n bytes of `str1` and `str2`
-*/
-int strncmp(const char* str1, const char *str2, size_t n)
+ */
+int strncmp(const char *str1, const char *str2, size_t n)
 {
     while (*str1 && *str2 && *str1 == *str2 && n) {
         str1++;
@@ -184,14 +171,13 @@ int strncmp(const char* str1, const char *str2, size_t n)
         n--;
     }
 
-    if (n == 0)
-        return 0;
-    return (u8)*str1 - (u8)*str2;
+    if (n == 0) return 0;
+    return (unsigned char)*str1 - (unsigned char)*str2;
 }
 
-char *strcat(char* dest, const char* src)
+char *strcat(char *dest, const char *src)
 {
-    char* ret = dest;
+    char *ret = dest;
     while (*dest)
         dest++;
     while (*src)
@@ -199,6 +185,8 @@ char *strcat(char* dest, const char* src)
     *dest = '\0';
     return ret;
 }
+
+void *memcpy(void *dest, const void *src, size_t n);
 
 /*
  * duplicate a string (malloc-based)
@@ -208,8 +196,7 @@ char *strdup(const char *str)
     size_t len = strlen(str);
 
     char *p = malloc(len + 1);
-    if (p == NULL)
-        return NULL;
+    if (p == 0) return 0;
 
     return memcpy(p, str, len + 1);
 }
@@ -221,8 +208,7 @@ char *strndup(const char *str, size_t n)
 {
     size_t len = strnlen(str, n);
     char *p = malloc(len + 1);
-    if (!p)
-        return NULL;
+    if (!p) return 0;
     memcpy(p, str, len);
     p[len] = '\0';
     return p;
@@ -231,11 +217,12 @@ char *strndup(const char *str, size_t n)
 /*
  * The `memset()` function fills the first `n` bytes of the
  * memory area pointed to by `dest` with the constant byte `c`
-*/
-void *memset(void *dest, int c, size_t n) {
-    u8 *p = dest;
+ */
+void *memset(void *dest, int c, size_t n)
+{
+    unsigned char *p = dest;
     while (n-- > 0)
-        *p++ = (u8) c;
+        *p++ = (unsigned char)c;
 
     return dest;
 }
@@ -245,11 +232,10 @@ void *memset(void *dest, int c, size_t n) {
  */
 int memcmp(const void *ptr1, const void *ptr2, size_t n)
 {
-    u8 *p1 = (u8 *)ptr1;
-    u8 *p2 = (u8 *)ptr2;
+    unsigned char *p1 = (unsigned char *)ptr1;
+    unsigned char *p2 = (unsigned char *)ptr2;
     while (n--) {
-        if (*p1 != *p2)
-            return *p1 - *p2;
+        if (*p1 != *p2) return *p1 - *p2;
         p1++;
         p2++;
     }
@@ -259,19 +245,19 @@ int memcmp(const void *ptr1, const void *ptr2, size_t n)
 /*
  * returns a pointer to the first occurrence of the uchar `c`
  * in the space start from `ptr`. If the pointer finder holds
- * run out of range, return NULL
-*/
+ * run out of range, return 0
+ */
 void *memchr(const void *ptr, int c, size_t n)
 {
-    u8 *p = (u8 *)ptr;
-    while (n--)
-    {
-        if (*p == (u8)c)
+    unsigned char *p = (unsigned char *)ptr;
+    while (n--) {
+        if (*p == (unsigned char)c) {
             return (void *)p;
+        }
         p++;
     }
 
-    return NULL;
+    return 0;
 }
 
 /*
@@ -281,7 +267,7 @@ void *memcpy(void *dest, const void *src, size_t n)
 {
     char *p = dest;
     while (n--)
-        *p++ = *(char*)src++;
+        *p++ = *(char *)src++;
 
     return dest;
 }
@@ -296,5 +282,3 @@ char *strerror(int errnum)
     char *str = __get_errstr(errnum);
     return str;
 }
-
-// TODO : Other functions in string.h
