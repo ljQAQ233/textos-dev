aba6249118270682f11d593f7390d65262516699
maouai233 <maouai233@outlook.com>
2026-04-05 13:42:14 +0800

    [fix] klib/bitmap : fixes memory leakage / applies perspicuous parameter names

--- src/include/textos/klib/bitmap.h
+++ src/include/textos/klib/bitmap.h
@@ -2,19 +2,21 @@
 
 // do not change it!
 #define mpunit u8
-#define mpfull (mpunit)-1
+#define mpfull (mpunit) - 1
 #define mpemsz sizeof(mpunit)
 #define mpembs (mpemsz * 8)
 
 typedef struct bitmap
 {
-    mpunit *map;
-    size_t siz;
+    mpunit *map; // bitmap itself
+    size_t bits; // size in bits
 } bitmap_t;
 
 bitmap_t *bitmap_init(bitmap_t *bmp, void *buf, size_t siz);
 
 size_t bitmap_find(bitmap_t *bmp);
+size_t bitmap_find_from(bitmap_t *bmp, size_t from);
+size_t bitmap_find_range(bitmap_t *bmp, size_t from, size_t to);
 
 bool bitmap_test(bitmap_t *bmp, size_t i);
 
@@ -24,4 +26,4 @@ void bitmap_reset(bitmap_t *bmp, size_t i);
 
 void bitmap_clear(bitmap_t *bmp);
 
-#define bitmap_exist(bmp, i) (bmp->siz > i)
+#define bitmap_exist(bmp, i) (bmp->bits > i)
