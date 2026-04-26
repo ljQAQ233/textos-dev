8a9603dd1fa8d3031ee89db12569184af4388192
maouai233 <maouai233@outlook.com>
2026-04-26 13:21:12 +0800

    [fix] libc : main is passed to __libc_start_main by crt, otherwise our       dynamic linker will take the main in the same file as its entrypoint

--- src/app/libc/crt1.c
+++ src/app/libc/crt1.c
@@ -1,3 +1,7 @@
+#include <sys/cdefs.h>
+
+int main(int argc, const char *argv[], const char *envp[]);
+
 __attribute__((weak))
 __attribute__((naked))
 void _start()
@@ -5,6 +9,7 @@ void _start()
     asm volatile(
         "movq %rsp, %rdi\n"
         "xorq %rbp, %rbp\n"
+        "leaq main(%rip), %rsi\n"
         "call __libc_start_main"
         );
 }
