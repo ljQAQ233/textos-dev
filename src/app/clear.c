da8187fe459064d795350e1e6c56c05ba9614f8a
maouai233 <maouai233@outlook.com>
2026-05-01 20:04:32 +0800

    [fix] app/clear : puts is used improperly

--- src/app/clear.c
+++ src/app/clear.c
@@ -2,7 +2,8 @@
 
 int main(int argc, char const *argv[])
 {
-    puts("\033[2J");   // clear screen
-    puts("\033[0;0f"); // cursor reset
+    printf("\033[2J");   // clear screen
+    printf("\033[0;0f"); // cursor reset
+    fflush(stdout);
     return 0;
 }
