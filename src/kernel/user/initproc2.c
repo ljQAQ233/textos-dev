6de985136fd51bfe23cde4cd3cb6700fc9e6dc9f
maouai233 <maouai233@outlook.com>
2026-05-04 18:39:35 +0800

    [upd] kernel : detach userspace preparation and jump from main.c

---
+++ src/kernel/user/initproc2.c
@@ -0,0 +1,36 @@
+/**
+ * @brief handle everything ready to switch to userspace, before jumping
+ * into user space, and then hand privileges to /bin/init
+ */
+#include <cpu.h>
+#include <textos/panic.h>
+#include <bits/a-sc.h>
+#include <bits/syscall.h>
+
+#define asm __asm__
+
+static void run_init(char *init)
+{
+    /*
+     * Now that we have a stack in user space, we can't call sys_execve
+     * directly, which causes a unrecoverable pagefault after switch pagetable
+     * (you can give it a shot). syscalls can switch to kernel stack first.
+     */
+    char *argv[] = {
+        init,
+        NULL,
+    };
+    __syscall(SYS_execve, argv[0], argv, 0, 0, 0, 0);
+}
+
+extern int close(int);
+
+void initproc2()
+{
+    close(0);
+    close(1);
+    close(2);
+
+    run_init("/bin/init");
+    PANIC("init exiting...\n");
+}
