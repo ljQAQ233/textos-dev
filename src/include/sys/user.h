611822e53d0ca8a1d1d88c859cb41e96a59e1f1b
maouai233 <maouai233@outlook.com>
2026-05-17 11:26:36 +0800

    fix(libc): add missing headfile `sys/user.h` and its internal files

---
+++ src/include/sys/user.h
@@ -0,0 +1,16 @@
+#ifndef _SYS_USER_H
+#define _SYS_USER_H
+
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
+#include <limits.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include <bits/user.h>
+
+__END_DECLS
+
+#endif
