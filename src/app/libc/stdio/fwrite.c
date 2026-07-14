3bb118ae93fb52105a459043479f3fcf2969b28a
maouai233 <maouai233@outlook.com>
2026-07-14 14:13:33 +0800

    fix(libc): stdio - _IONBF makes no sense due to ignorance of ibl = EOF

--- src/app/libc/stdio/fwrite.c
+++ src/app/libc/stdio/fwrite.c
@@ -1,28 +1,27 @@
 #include "stdio.h"
 #include <string.h>
 
-size_t __fwritex(const unsigned char *restrict buf, size_t len, FILE *restrict f)
+size_t __fwritex(const unsigned char *restrict buf, size_t len,
+                 FILE *restrict f)
 {
     size_t i = 0;
-    if (!f->wend && __towrite(f))
-        return 0;
-
+    if (!f->wend && __towrite(f)) return 0;
     // 缓冲区满, 输出 buf + 缓冲区
-    if (len > f->wend - f->wpos)
-        return f->write(f, buf, len);
+    if (len > f->wend - f->wpos) return f->write(f, buf, len);
     // 检查换行符, 输出
-    if (f->lbf >= 0)
-    {
-        for (i = len; i && buf[i-1] != '\n'; i--);
+    if (f->lbf >= 0) {
+        for (i = len; i && buf[i - 1] != '\n'; i--)
+            ;
         if (i) {
             // 输出换行符前的一段
             // n < i 说明不能再写了, 返回
             size_t n = f->write(f, buf, i);
-            if (n < i)
-                return n;
+            if (n < i) return n;
             buf += i;
             len -= i;
         }
+    } else {
+        return f->write(f, buf, len);
     }
 
     memcpy(f->wpos, buf, len);
@@ -30,10 +29,10 @@ size_t __fwritex(const unsigned char *restrict buf, size_t len, FILE *restrict f
     return len + i;
 }
 
-size_t fwrite(const void *restrict ptr, size_t size, size_t nmemb, FILE *restrict f)
+size_t fwrite(const void *restrict ptr, size_t size, size_t nmemb,
+              FILE *restrict f)
 {
-    if (!size || !nmemb)
-        return 0;
+    if (!size || !nmemb) return 0;
     size_t l = size * nmemb;
     size_t i = __fwritex(ptr, l, f);
     return i == l ? nmemb : i / size;
