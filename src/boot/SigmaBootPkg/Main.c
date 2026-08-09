3fbbde27b35c7b4d567332aa33bc54fe97822f01
maouai233 <maouai233@outlook.com>
2026-08-09 15:43:53 +0800

    feat!(efi): MemoryGetMap moved to Main.c

--- src/boot/SigmaBootPkg/Main.c
+++ src/boot/SigmaBootPkg/Main.c
@@ -13,7 +13,51 @@
 #include <Graphics.h>
 #include <Kernel.h>
 #include <Logo.h>
-#include <Memory.h>
+
+EFI_STATUS
+GetMemoryMap (
+  MAP_INFO  *Info
+  )
+{
+  ASSERT (Info != NULL);
+
+  EFI_STATUS  Status  = EFI_SUCCESS;
+  VOID        *Descs  = NULL;
+  UINTN       MapSiz  = 0;
+  UINTN       PageNum = 0;
+
+Retry:
+  Status = gBS->GetMemoryMap (
+                  &MapSiz,
+                  Descs,
+                  &Info->MapKey,
+                  &Info->DescSize,
+                  &Info->DescVersion
+                  );
+  if (Status == EFI_BUFFER_TOO_SMALL) {
+    if (Descs != NULL) {
+      gBS->FreePages ((EFI_PHYSICAL_ADDRESS)Descs, PageNum);
+    }
+
+    PageNum = EFI_SIZE_TO_PAGES (MapSiz);
+    Status  = gBS->AllocatePages (
+                     AllocateAnyPages,
+                     EfiReservedMemoryType,
+                     PageNum,
+                     (EFI_PHYSICAL_ADDRESS *)&Descs
+                     );
+    ERR_RETS (Status);
+
+    goto Retry;
+  }
+
+  ERR_RETS (Status);
+
+  Info->Descs    = Descs;
+  Info->MapSize  = MapSiz;
+  Info->MapCount = Info->MapSize / Info->DescSize;
+  return Status;
+}
 
 EFI_STATUS
 ExitBootServices (
@@ -23,7 +67,7 @@ ExitBootServices (
 {
   EFI_STATUS  Status = EFI_SUCCESS;
 
-  Status = MemoryGetMap (Info);
+  Status = GetMemoryMap (Info);
   ERR_RETS (Status);
 
   Status = gBS->ExitBootServices (ImageHandle, Info->MapKey);
--- src/include/boot/Memory.h
+++ src/include/boot/Memory.h
@@ -1,11 +1,4 @@
 #ifndef __MEMORY_H__
 #define __MEMORY_H__
 
-#include <Page.h>
-
-EFI_STATUS
-MemoryGetMap (
-  MAP_INFO  *Info
-  );
-
 #endif
