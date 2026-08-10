472971ca05703e728c2c855e01a2bad123a1dc1e
maouai233 <maouai233@outlook.com>
2026-08-10 18:30:23 +0800

    feat!(arch): split __common64 from __multi64, __efi64 added
    NOTE that __efi64 is unusable!!! because missing load_base
    and load_size, or further, a "load_offset", which is the minimum
    physical address of PT_LOADs in our SigmaBoot OS loader

--- src/kernel/arch/x86_64/arch.mk
+++ src/kernel/arch/x86_64/arch.mk
@@ -1,7 +1,10 @@
-
-SRCS += $(ARCH_DIR)/boot/_start.S
-SRCS += $(ARCH_DIR)/boot/multi.S
+SRCS += $(ARCH_DIR)/boot/common.c
+SRCS += $(ARCH_DIR)/boot/efi.c
+SRCS += $(ARCH_DIR)/boot/efi.S
 SRCS += $(ARCH_DIR)/boot/multi.c
+SRCS += $(ARCH_DIR)/boot/multi.S
+SRCS += $(ARCH_DIR)/boot/_start.S
+
 SRCS += $(ARCH_DIR)/cpu.S
 SRCS += $(ARCH_DIR)/ap.c
 SRCS += $(ARCH_DIR)/ap.S
@@ -14,4 +17,6 @@ SRCS += $(ARCH_DIR)/irq.S
 SRCS += $(ARCH_DIR)/time.c
 SRCS += $(ARCH_DIR)/ptrace.c
 
-$(BUILD)/$(ARCH_DIR)/multi.c.o: CFLAGS += -fno-asynchronous-unwind-tables
+$(BUILD)/$(ARCH_DIR)/boot/multi.c.o: CFLAGS += -fno-asynchronous-unwind-tables
+$(BUILD)/$(ARCH_DIR)/boot/common.c.o: CFLAGS += -fno-asynchronous-unwind-tables
+$(BUILD)/$(ARCH_DIR)/boot/efi.c.o: CFLAGS += -fno-asynchronous-unwind-tables
---
+++ src/kernel/arch/x86_64/boot/efi.c
@@ -0,0 +1,86 @@
+#include <textos/boot.h>
+#include <textos/user/elf.h>
+#include <textos/boot/efi.h>
+#include <textos/uefi.h>
+
+#include "common.h"
+#include "textos/uefi/mm.h"
+
+static __bdata void *descs;
+static __bdata size_t desccnt;
+static __bdata size_t descsiz;
+
+#define to_len(pgs) (EFI_PAGE_SIZE * (pgs))
+#define to_pgs(len) ((len) / EFI_PAGE_SIZE)
+
+static __bcode void reskern()
+{
+    addr_t kern_start = lbase;
+    addr_t kern_end   = align_up(lend, PAGE_SIZE);
+
+    for (int i = 0; i < desccnt; i++)
+    {
+        EFI_MEMORY_DESCRIPTOR *desc = (void *)descs + descsiz * i;
+        addr_t start = desc->PhysicalStart;
+        addr_t end   = desc->PhysicalStart + to_len(desc->NumberOfPages);
+        if (end <= kern_start || start >= kern_end)
+            continue;
+
+        if (start < kern_start && end > kern_end)
+        {
+            // map 跨过内核，分成两段
+            // 先保留低地址段
+            desc->NumberOfPages = kern_start - start;
+            EFI_MEMORY_DESCRIPTOR *newmap = (EFI_MEMORY_DESCRIPTOR *)descs;
+            newmap->PhysicalStart = kern_end;
+            newmap->NumberOfPages = to_pgs(end - kern_end);
+            newmap->Type = desc->Type;
+            newmap->Attribute = desc->Attribute;
+        }
+        else if (start >= kern_start && end <= kern_end)
+        {
+            // 整个 map 都被内核占用
+            desc->PhysicalStart = 0;
+            desc->NumberOfPages = 0;
+        }
+        else if (start < kern_start && end > kern_start)
+        {
+            // map 末尾和内核重叠
+            desc->NumberOfPages = to_pgs(kern_start - start);
+        }
+        else if (start < kern_end && end > kern_end)
+        {
+            // map 起始和内核重叠
+            desc->PhysicalStart = kern_end;
+            desc->NumberOfPages  = to_pgs(end - kern_end);
+        }
+    }
+}
+
+static __bcode void *getpage()
+{
+    EFI_MEMORY_DESCRIPTOR *map = (void *)descs;
+    for (int i = 0; i < desccnt; i++, map++)
+    {
+        if (!map->PhysicalStart || !map->NumberOfPages)
+            continue;
+        if (map->Type == EfiConventionalMemory)
+        {
+            addr_t res = map->PhysicalStart;
+            map->PhysicalStart += to_len(1);
+            map->NumberOfPages -= 1;
+            return (void *)res;
+        }
+    }
+    return NULL;
+}
+
+__bcode void __efi64(long magic, long info)
+{
+    mapinfo_t *i = ((bconfig_t *)info)->mapinfo;
+    descs = i->map;
+    desccnt = i->desccnt;
+    descsiz = i->descsiz;
+    reskern();
+    __common64(magic, info, getpage);
+}
