81990d451a403bb6396934b59af079765f3e73c2
maouai233 <maouai233@outlook.com>
2026-08-18 22:12:32 +0800

    feat(kernel/dev): add _init/fini_pctx

--- src/kernel/fs/dev.c
+++ src/kernel/fs/dev.c
@@ -17,8 +17,7 @@ static inline devst_t *dev_extract(node_t *node)
 static int dev_ioctl(node_t *this, int req, void *argp, struct fs_openctx *openctx)
 {
     devst_t *dev = dev_extract(this);
-    if (!dev)
-        return -EINVAL;
+    if (!dev) return -EINVAL;
     return dev->ioctl(dev, req, argp);
 }
 
@@ -26,9 +25,8 @@ static int dev_read(node_t *this, void *buf, size_t siz, size_t offset,
                     struct fs_openctx *openctx)
 {
     devst_t *dev = dev_extract(this);
-    if (!dev)
-        return -EINVAL;
-    if (dev->type == DEV_CHAR)
+    if (!dev) return -EINVAL;
+    if (dev->type == DEV_CHAR) // chardev
         return dev->read(dev, buf, siz);
 
     int blksiz;
@@ -58,9 +56,8 @@ static int dev_write(node_t *this, void *buf, size_t siz, size_t offset,
                      struct fs_openctx *openctx)
 {
     devst_t *dev = dev_extract(this);
-    if (!dev)
-        return -EINVAL;
-    if (dev->type == DEV_CHAR)
+    if (!dev) return -EINVAL;
+    if (dev->type == DEV_CHAR) // chardev
         return dev->write(dev, buf, siz);
 
     int blksiz;
@@ -88,26 +85,41 @@ static int dev_close(node_t *this)
     return 0;
 }
 
+static int dev_init_pctx(node_t *this, void **pctx)
+{
+    devst_t *dev = dev_extract(this);
+    if (!dev) return -EINVAL;
+    if (!dev->_init_pctx) return 0;
+    return dev->_init_pctx(dev, pctx);
+}
+
+static int dev_fini_pctx(node_t *this, void **pctx)
+{
+    devst_t *dev = dev_extract(this);
+    if (!dev) return -EINVAL;
+    if (!dev->_fini_pctx) return 0;
+    return dev->_fini_pctx(dev, pctx);
+}
+
 static void *dev_mmap(node_t *this, vm_region_t *vm, struct fs_openctx *openctx)
 {
     devst_t *dev = dev_extract(this);
-    if (!dev)
-        return MRET(-EINVAL);
+    if (!dev) return MRET(-EINVAL);
     return dev->mmap(dev, vm);
 }
 
 fs_opts_t __vfs_dev_op = {
-    NULL,       /* open */
-    dev_close,  /* close */
-    NULL,       /* _init_pctx */
-    NULL,       /* _fini_pctx */
-    NULL,       /* mknod */
-    NULL,       /* chown */
-    NULL,       /* chmod */
-    NULL,       /* remove */
-    NULL,       /* readdir */
-    NULL,       /* seekdir */
-    NULL,       /* truncate */
+    .open = NULL, /* open */
+    dev_close,    /* close */
+    dev_init_pctx,
+    dev_fini_pctx,
+    NULL, /* mknod */
+    NULL, /* chown */
+    NULL, /* chmod */
+    NULL, /* remove */
+    NULL, /* readdir */
+    NULL, /* seekdir */
+    NULL, /* truncate */
     dev_read,
     dev_write,
     dev_mmap,
