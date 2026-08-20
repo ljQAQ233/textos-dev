22e9ccc6a4373af878421426f345af07d1725bef
maouai233 <maouai233@outlook.com>
2026-08-20 11:27:32 +0800

    refactor(kernel/dev): event - split client from registries

--- src/include/textos/dev/event.h
+++ src/include/textos/dev/event.h
@@ -2,8 +2,25 @@
 
 #include <bits/event.h>
 
-devst_t *event_register(enum event_type type);
-void event_deliver(devst_t *this);
-void event_push_any(devst_t *this, struct event *ev);
-void event_push_keyboard(devst_t *this, keysym_t sym);
-void event_push_mouse(devst_t *this, keysym_t status, int dx, int dy);
+struct task;
+struct devst;
+
+struct event_client
+{
+    struct task *waiter;
+    struct event ev;
+};
+
+struct event_registry
+{
+    struct devst *evdev;
+    struct event_client *client;
+};
+
+void event_deliver(struct event_registry *this);
+void event_push_any(struct event_registry *this, struct event *ev);
+void event_push_keyboard(struct event_registry *this, keysym_t sym);
+void event_push_mouse(struct event_registry *this, keysym_t status, int dx,
+                      int dy);
+
+struct event_registry *event_register(enum event_type type);
