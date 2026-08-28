4951fa85cbd974b35cc5684edfa51db9e8c5085c
maouai233 <maouai233@outlook.com>
2026-08-28 21:31:14 +0800

    fix(textos/klib/fifo.h): fix missing function declaration

--- src/include/textos/klib/fifo.h
+++ src/include/textos/klib/fifo.h
@@ -9,12 +9,9 @@ typedef struct
     size_t tail;
 } fifo_t;
 
-void fifo_clear(fifo_t *fifo);
-
-bool fifo_full(fifo_t *fifo);
-
 fifo_t *fifo_init(fifo_t *fifo, char *buf, size_t siz);
-
-size_t fifo_read(fifo_t *fifo, char *buf, size_t siz);
-
 size_t fifo_write(fifo_t *fifo, char *data, size_t siz);
+size_t fifo_read(fifo_t *fifo, char *buf, size_t siz);
+bool fifo_full(fifo_t *fifo);
+bool fifo_empty(fifo_t *fifo);
+void fifo_clear(fifo_t *fifo);
