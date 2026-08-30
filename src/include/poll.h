de9a3920a1cb2ec36061be86939f69983da828d8
maouai233 <maouai233@outlook.com>
2026-08-30 21:02:35 +0800

    chore: add `sys/poll.h`

--- src/include/poll.h
+++ src/include/poll.h
@@ -1,8 +1,14 @@
 #ifndef _POLL_H
 #define _POLL_H
 
+#include <sys/cdefs.h>
+
+__BEGIN_DECLS
+
 #include <bits/poll.h>
 
 int poll(struct pollfd __fds[], nfds_t __nfds, int __timeout /* ms */);
+
+__END_DECLS
 
 #endif
---
+++ src/include/sys/poll.h
@@ -0,0 +1,7 @@
+#ifndef _SYS_POLL_H
+#define _SYS_POLL_H
+
+// POSIX poll
+#include <poll.h>
+
+#endif
