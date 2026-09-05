df8a39b2f77935d5517337304dfc44671883520c
maouai233 <maouai233@outlook.com>
2026-09-05 21:32:23 +0800

    feat(app/cat): more detailed error message

--- src/app/cat.c
+++ src/app/cat.c
@@ -1,5 +1,9 @@
 #include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
+
+#include "util.h"
+PROG_NAME("cat");
 
 #define N 4096
 char buf[N];
@@ -8,27 +12,31 @@ int fcat(int fd)
 {
     ssize_t n;
     while ((n = read(fd, buf, N)) > 0)
-        write(1, buf, n);
-    return 0;
+        if (write(1, buf, n) <= 0) break;
+    return n < 0 ? -1 : 0;
 }
 
 int cat(const char *file)
 {
     int fd = open(file, O_RDONLY);
-    if (fd < 0) return 1;
-    fcat(fd);
+    if (fd < 0) {
+        error_x(file);
+        return 1;
+    }
+    int ret = fcat(fd);
+    if (ret) error_x(file);
     close(fd);
-    return 0;
+    return ret ? 1 : 0;
 }
 
 int main(int argc, char *argv[])
 {
-    int ret = 0;
+    int err = 0;
     if (argc == 1) {
-        ret |= fcat(0);
+        err |= fcat(0);
     } else {
         for (int i = 1; i < argc; i++)
-            ret |= cat(argv[i]);
+            err |= cat(argv[i]);
     }
-    return ret;
+    return !!err;
 }
