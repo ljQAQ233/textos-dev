af7b5f719814df8dfd636a55930668d748eac3d6
maouai233 <maouai233@outlook.com>
2026-09-08 23:42:53 +0800

    feat(bits/null.h): defensive programming - avoid redefinition of NULL

--- src/include/bits/null.h
+++ src/include/bits/null.h
@@ -1,10 +1,12 @@
 /*
  * NULL may be defined in many headfiles of libc
  */
-#if __cplusplus >= 201103L
-  #define NULL nullptr
-#elif defined(__cplusplus)
-  #define NULL 0L
-#else
-  #define NULL ((void*)0)
+#if !defined(NULL)
+    #if __cplusplus >= 201103L
+        #define NULL nullptr
+    #elif defined(__cplusplus)
+        #define NULL 0L
+    #else
+        #define NULL ((void *)0)
+    #endif
 #endif
